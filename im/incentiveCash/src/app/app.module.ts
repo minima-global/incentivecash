70a16e6e4227f3c88982a1e7639bfb0a8465d48f
eliasnemr <eliasnemr@gmail.com>
2021-02-10 13:40:16 +0200

    template of login ready

--- im/incentiveCash/src/app/app.module.ts
+++ im/incentiveCash/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -10,7 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- im/incentiveCash/src/app/home/home.page.ts
+++ im/incentiveCash/src/app/home/home.page.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Minima } from 'minima';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  {
 
-  constructor() {}
+  public loginForm: FormGroup;
+
+  constructor(public formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      uName: '',
+      pKey: ''
+    });
+  }
+
+  getReferenceID() {
+
+    alert('Your reference ID is: 0x00');
+
+    const referenceID = '0x00'
+    // Minima.net.get to get the reference ID
+
+    const userDetails = { referenceID: referenceID }
+
+    Minima.file.save(JSON.stringify(userDetails), 'userDetails.txt', (res: any) => {
+      if (res.success) {
+        console.log('Saved reference id');
+
+        // router Link to the incentive cash list
+
+      } else {
+        console.log('Failed to save reference id.');
+      }
+    });
+
+  }
+
+  get username() {
+    return this.loginForm.get('uName');
+  }
+
+  get publickey() {
+    return this.loginForm.get('pKey');
+  }
 
 }
