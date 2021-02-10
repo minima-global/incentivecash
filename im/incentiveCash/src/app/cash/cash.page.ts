be2a98032618c05a5cf961f0a03ef374ca2f2be1
eliasnemr <eliasnemr@gmail.com>
2021-02-10 17:24:04 +0200

    cashPage template set, dummy list added

--- im/incentiveCash/src/app/cash/cash.page.ts
+++ im/incentiveCash/src/app/cash/cash.page.ts
@@ -7,11 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CashPage implements OnInit {
 
+  cashdummy = [
+    {index: '#1', date:'07 - 02, 2021', status:'<span fill="clear">Collect</span>'},
+    {index: '#2', date:'14 - 02, 2021', status:'<span fill="clear">Collect</span>'},
+    {index: '#3', date:'28 - 02, 2021', status:'<span fill="clear">Collect</span>'},
+    {index: '#4', date:'7 - 03, 2021', status:'<span fill="clear">Collect</span>'},
+  ]
   cashlist = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  collectCash() {
+    
   }
 
 }
